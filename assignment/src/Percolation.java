0ff8e91b1cf59560bee9bc75cecedbbc6bd563e1
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2018-01-21 11:13:47 +0000

    Percolation assignment -- passed

--- assignment/src/Percolation.java
+++ assignment/src/Percolation.java
@@ -1,154 +1,216 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+/**
+ * @author Laszlo Bogacsi
+ * @since 20/01/2018
+ *
+ * The percolation class implements the weighted union find algorithm-s API
+ * in order to find out if a given system percolates or not.
+ * Initializes a N*N grid with all-blocked sites, then by opening a random site
+ * one by one can check whether the system percolates.
+ */
 
 public class Percolation {
-    int gridSize;  // size of a square grid
-    int[] openSites;  // 0 closed, 1, open
-    int numberOfOpenSites; // counter for how many sites are open
-    WeightedQuickUnionUF weightedQuickUnionUF;
-    WeightedQuickUnionUF weightedQuickUnionUFNoBackwash;
-    int currSite;
-    int virtualTopNode;
-    int virtualBottomNode;
+    // size of a square grid
+    private final int gridSize;
+    // array to track opened sites, values: false blocked, true open
+    private final boolean[] openSites;
+    // counter for how many sites are open
+    private int numberOfOpenSites;
+    // the main quic union find object with 2 virtual nodes to check if the
+    // system percolates
+    private final WeightedQuickUnionUF wQUUF;
+    /* to work around the backwash problem, this wQUF object has only the top
+     * virtual node, used only to tell if a site is full
+     */
+    private final WeightedQuickUnionUF wQUUFNoBackwash;
+    // the id of the top virtual node ( last site +1 )
+    private final int vTopNode;
+    // the id of the bottom virtual node ( last site +2 )
+    private final int vBottomNode;
 
-    // create n-by-n grid, with all sites blocked
+    /**
+     * The constructor initializes the grid and the blocked sites,
+     * creates an array to track open sites,
+     * creates two virtual nodes,
+     * instantiates the weighted quick union find object.
+     * @exception  IllegalArgumentException on invalid grid-size (less than
+     * or equal to 0
+     * @param n size of the grid
+     */
     public Percolation(int n) {
-        if (n <= 0) throw new IllegalArgumentException("Grid size must be positive integer");
+        if (n <= 0) {
+            throw new IllegalArgumentException(
+                    "Grid size must be positive integer");
+        }
         gridSize = n;
-        openSites = new int[gridSize * gridSize+2];
-        virtualTopNode  = gridSize * gridSize ;
-        virtualBottomNode  = gridSize * gridSize + 1;
-        weightedQuickUnionUF = new WeightedQuickUnionUF(gridSize * gridSize + 2);
-        weightedQuickUnionUFNoBackwash = new WeightedQuickUnionUF(gridSize * gridSize + 1);
+        openSites = new boolean[gridSize * gridSize+2];
+        vTopNode = gridSize * gridSize ;
+        vBottomNode = gridSize * gridSize + 1;
+        wQUUF = new WeightedQuickUnionUF(gridSize * gridSize + 2);
+        wQUUFNoBackwash = new WeightedQuickUnionUF(gridSize * gridSize + 1);
 
         numberOfOpenSites = 0;
         for (int i = 0; i < n * n+2 ; i++) {
-            openSites[i] = 0;
+            openSites[i] = false;
         }
-        openSites[virtualTopNode] = 1;
-        openSites[virtualBottomNode] = 1;
+        // mark the virtual top and bottom nodes open
+        openSites[vTopNode] = true;
+        openSites[vBottomNode] = true;
     }
-    // open site (row, col) if it is not open already
+
+    /**
+     * Opens a site if it's not already open and connects the site to
+     * it's 4 adjacent neighbours (left, right, above, below)
+     * @param row row number of the site (first row is 1 last is n)
+     *            numbering starts at the top left corner
+     * @param col column nuber of the site (first column is 1 last is n)
+     *            numbering starts at the top left corner
+     */
     public void open(int row, int col) {
         validate(row, col);
-        currSite = xyTo1D(row, col);
+        int currSite = xyTo1D(row, col);
 
         // if site is not open, then open it
-        if(!isOpen(row, col)){
-            openSites[currSite] = 1;
+        if (!isOpen(row, col)){
+            openSites[currSite] = true;
             numberOfOpenSites ++;
         } else {
             return;
         }
-        // Try connect site to neighbours
-        //Left neighbour
+        // upon first row, automatically connect to virtual top node
         if (row == 1) {
-            connectNeighbourIfPossible(weightedQuickUnionUF, currSite, virtualTopNode);
-            connectNeighbourIfPossible(weightedQuickUnionUFNoBackwash, currSite, virtualTopNode);
+            connectNeighbour(wQUUF, currSite, vTopNode);
+            connectNeighbour(wQUUFNoBackwash, currSite, vTopNode);
 
         }
-        if (row == gridSize) connectNeighbourIfPossible(weightedQuickUnionUF, currSite, virtualBottomNode);
+        // upon last row, automatically connect to virtual bottom node
+        if (row == gridSize) connectNeighbour(wQUUF, currSite, vBottomNode);
+        // Left neighbour
         if ((col - 1 > 0) && isOpen(row, col -1 )) {
             int leftSite = xyTo1D(row, col -1);
-            connectNeighbourIfPossible(weightedQuickUnionUF, currSite, leftSite);
-            connectNeighbourIfPossible(weightedQuickUnionUFNoBackwash, currSite, leftSite);
+            connectNeighbour(wQUUF, currSite, leftSite);
+            connectNeighbour(wQUUFNoBackwash, currSite, leftSite);
         }
-        //Top neighbour
+        // Top neighbour
         if ((row - 1 > 0) && isOpen(row-1, col )) {
             int topSite = xyTo1D(row -1, col);
-            connectNeighbourIfPossible(weightedQuickUnionUF, currSite, topSite);
-            connectNeighbourIfPossible(weightedQuickUnionUFNoBackwash, currSite, topSite);
+            connectNeighbour(wQUUF, currSite, topSite);
+            connectNeighbour(wQUUFNoBackwash, currSite, topSite);
         }
-        //Right neighbour
+        // Right neighbour
         if ((col + 1  <= gridSize) && isOpen(row, col + 1 )) {
             int rightSite = xyTo1D(row, col + 1);
-            connectNeighbourIfPossible(weightedQuickUnionUF, currSite, rightSite);
-            connectNeighbourIfPossible(weightedQuickUnionUFNoBackwash, currSite, rightSite);
+            connectNeighbour(wQUUF, currSite, rightSite);
+            connectNeighbour(wQUUFNoBackwash, currSite, rightSite);
         }
-        //Bottom neighbour
+        // Bottom neighbour
         if ((row + 1 <= gridSize) && isOpen(row + 1, col)) {
             int bottomtSite = xyTo1D(row + 1, col);
-            connectNeighbourIfPossible(weightedQuickUnionUF, currSite, bottomtSite);
-            connectNeighbourIfPossible(weightedQuickUnionUFNoBackwash, currSite, bottomtSite);
+            connectNeighbour(wQUUF, currSite, bottomtSite);
+            connectNeighbour(wQUUFNoBackwash, currSite, bottomtSite);
         }
     }
 
     // is site (row, col) open?
+
+    /**
+     * The isOpen method determines whether a site is open or not
+     * based on the sites position in the grid.
+     * @param row row number of the site (first row is 1 last is n)
+     *            numbering starts at the top left corner
+     * @param col column nuber of the site (first column is 1 last is n)
+     *            numbering starts at the top left corner
+     * @return true if open, false if blocked
+     */
     public boolean isOpen(int row, int col) {
-        return openSites[xyTo1D(row, col)] == 1;
+        return openSites[xyTo1D(row, col)];
     }
 
-    // is site (row, col) full?
+    /**
+     * The isFull method determines whether a site is full or not
+     * based on the sites position in the grid.
+     * Full if connected to top virtual node.
+     * @param row row number of the site (first row is 1 last is n)
+     *            numbering starts at the top left corner
+     * @param col column nuber of the site (first column is 1 last is n)
+     *            numbering starts at the top left corner
+     * @return true if full, false if empty (but open)
+     */
     public boolean isFull(int row, int col) {
         int site = xyTo1D(row, col);
-        return  isOpen(row, col) && weightedQuickUnionUFNoBackwash.connected(site, virtualTopNode);
+        return isOpen(row, col) && wQUUFNoBackwash.connected(site, vTopNode);
     }
 
     // number of open sites
+
+    /**
+     * This method returns the number of currently open sites
+     * @return number of open sites (int)
+     */
     public int numberOfOpenSites() {
         return numberOfOpenSites;
     }
 
-    // does the system percolate?
+    /**
+     * Determines whether the system percolates
+     * A system percolates if a site in the top row is connected
+     * to a site in the bottom row.
+     * @return true if percolates, false if not.
+     */
     public boolean percolates() {
-        return weightedQuickUnionUF.connected(virtualTopNode, virtualBottomNode);
+        return wQUUF.connected(vTopNode, vBottomNode);
     }
 
+    /**
+     * mmain method for testing
+     * @param args currently unused
+     */
     public static void main(String[] args) {
-        Percolation percolation = new Percolation(5);
-        int row, col;
-        row = 1;
-        col = 2;
-        percolation.open(row, col);
-        System.out.println("isOpen? " + percolation.isOpen(row, col));
-        System.out.println("isFull? " + percolation.isFull(row, col));
-        System.out.println("does it percolate? " + percolation.percolates());
-        System.out.println("num of open sites: " + percolation.numberOfOpenSites());
-        System.out.println("---------------------------------");
-        row = 2;
-        percolation.open(row, col);
-        System.out.println("isOpen? " + percolation.isOpen(row, col));
-        System.out.println("isFull? " + percolation.isFull(row, col));
-        System.out.println("does it percolate? " + percolation.percolates());
-        System.out.println("num of open sites: " + percolation.numberOfOpenSites());
-        System.out.println("---------------------------------");
-        row = 3;
-        percolation.open(row, col);
-        System.out.println("isOpen? " + percolation.isOpen(row, col));
-        System.out.println("isFull? " + percolation.isFull(row, col));
-        System.out.println("does it percolate? " + percolation.percolates());
-        System.out.println("num of open sites: " + percolation.numberOfOpenSites());
-        System.out.println("---------------------------------");
-        row = 4;
-        percolation.open(row, col);
-        System.out.println("isOpen? " + percolation.isOpen(row, col));
-        System.out.println("isFull? " + percolation.isFull(row, col));
-        System.out.println("does it percolate? " + percolation.percolates());
-        System.out.println("num of open sites: " + percolation.numberOfOpenSites());
-        System.out.println("---------------------------------");
-        row = 5;
-        percolation.open(row, col);
-        System.out.println("isOpen? " + percolation.isOpen(row, col));
-        System.out.println("isFull? " + percolation.isFull(row, col));
-        System.out.println("does it percolate? " + percolation.percolates());
-        System.out.println("num of open sites: " + percolation.numberOfOpenSites());
-        System.out.println("---------------------------------");
-
+        // optional tests here
     }
 
-    // translate 2D coordinates to 1D pointer
+    /**
+     * Private method to translate 2D coordinates(row column) to
+     * 1D coordinate (array index) and validates the input coordinates.
+     * @param row row number of the site (first row is 1 last is n)
+     *            numbering starts at the top left corner
+     * @param col column nuber of the site (first column is 1 last is n)
+     *            numbering starts at the top left corner
+     * @return an int starting from 0 (for array index)
+     */
     private int xyTo1D(int row, int col) {
         validate(row, col);
         return (row - 1) * gridSize + col - 1;
     }
 
+    /**
+     * This method validates a pair of site coordinates
+     * to check if they fall inside or outside the grid
+     * @exception IllegalArgumentException on coordinates
+     * that fall outside the grid.
+     * @param row row number of the site (first row is 1 last is n)
+     *            numbering starts at the top left corner
+     * @param col column nuber of the site (first column is 1 last is n)
+     *            numbering starts at the top left corner
+     */
     private void validate(int row, int col){
-        if((row <= 0 || row > gridSize ) || (col <= 0 || col > gridSize )) {
-            throw new IllegalArgumentException("The index is out of the grid");
+        if ((row <= 0 || row > gridSize ) || (col <= 0 || col > gridSize )) {
+            throw new IllegalArgumentException( "The index is out of the grid" );
         }
     }
 
-    private void connectNeighbourIfPossible(WeightedQuickUnionUF uf , int currSite, int neighbourSite){
+    /**
+     * convenience method for re-usability. Calls the union method on the
+     * weightedQuickUnionFind object to connect two sites if they are not
+     * already connected.
+     * @param uf WeightedQuickUnionFind object
+     * @param currSite the id of the current site
+     * @param neighbourSite the id of the adjacent neighbour site
+     *                      (try to connect the current site to this)
+     */
+    private void connectNeighbour(WeightedQuickUnionUF uf,
+                                  int currSite, int neighbourSite){
         if (!uf.connected(currSite, neighbourSite)) {
             uf.union(neighbourSite, currSite);
         }
--- assignment/src/PercolationStats.java
+++ assignment/src/PercolationStats.java
@@ -1,11 +1,34 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
+/**
+ * @author Laszlo Bogacsi
+ * @since 20/01/2018
+ *
+ * The PercolationStats class implements statistical methods to compute
+ * an estimated percolation threshold (trialsm time on an n*n grid)
+ */
 public class PercolationStats {
-    double[] results;
-    // perform trials independent experiments on an n-by-n grid
+    /* array of doubles to store the percolation results
+        the number of open sites when the system percolates.
+     */
+    private final double[] results;
+    private static final double CONFIDENCE_95 = 1.96;
+
+    /**
+     * The constructor performs the Percolation experiment 'trials' times on an
+     * n * n grid and pushes the result in to an array of doubles.
+     * @param n grid size, positive integer
+     * @param trials number of time to run the experiment
+     * @exception IllegalArgumentException on invald grid size or invalid trial
+     * number
+     */
     public PercolationStats(int n, int trials) {
-        if (n <= 0 || trials <= 0) throw new IllegalArgumentException("n or trials should be a positive integer");
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException(
+                    "n and trials should be a positive integer");
+        }
         results = new double[trials];
         for (int i = 0; i < trials; i++) {
             Percolation percolation = new Percolation(n);
@@ -17,35 +40,57 @@ public class PercolationStats {
             results[i] = percolation.numberOfOpenSites() / (n * n * 1.0);
         }
     }
-    // sample mean of percolation threshold
+
+    /**
+     * sample mean of percolation threshold
+     * @return double mean of the results array
+     */
     public double mean() {
         return StdStats.mean(results);
     }
 
-    // sample standard deviation of percolation threshold
+    /**
+     * sample standard deviation of percolation threshold
+     * @return double standard deviation of the results array
+     */
     public double stddev() {
         return StdStats.stddev(results);
-
     }
 
-    // low  endpoint of 95% confidence interval
+
+    /**
+     * low  endpoint of 95% confidence interval
+     * @return double of lower 95% confidence level
+     */
     public double confidenceLo() {
-        return mean() - (1.96*stddev()/Math.sqrt(results.length));
+        return mean() - (CONFIDENCE_95 * stddev() / Math.sqrt(results.length));
 
     }
 
-    // high endpoint of 95% confidence interval
+
+    /**
+     * high endpoint of 95% confidence interval
+     * @return double of higher 95% confidence level
+     */
     public double confidenceHi() {
-        return mean() + (1.96*stddev()/Math.sqrt(results.length));
+        return mean() + (CONFIDENCE_95 * stddev() / Math.sqrt(results.length));
 
     }
 
+    /**
+     * main method to test the PercolationStats class
+     * and print the mean, standard deviation and the 95% high and low
+     * confidence levels of the percolation threshold to the standard output.
+     * @param args first argument is the gridsize,
+     *             the second argument is the number of times
+     *             to repeat the experiment.
+     */
     public static void main(String[] args) {
-//        int n = Integer.parseInt(args[0]);
-//        int trials = Integer.parseInt(args[1]);
-        PercolationStats percolationStats = new PercolationStats(20, 1000);
-        System.out.println("mean:                    = "+ percolationStats.mean());
-        System.out.println("stddev:                  = " + percolationStats.stddev());
-        System.out.println("95% confidence interval: = [ "+ percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi() +" ]");
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats percolationStats = new PercolationStats(n, trials);
+        StdOut.printf("mean:                    = %s\n", percolationStats.mean());
+        StdOut.printf("stddev:                  = %s\n", percolationStats.stddev());
+        StdOut.printf("95%% confidence interval: = [%s, %s]", percolationStats.confidenceLo(), percolationStats.confidenceHi());
     }
 }
